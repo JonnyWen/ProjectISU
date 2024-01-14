d2cb989594146c76aab239069028c15eb222bfe4
J W <jw@Js-MacBook-Air.local>
2024-01-13 20:44:25 -0500

    Added Face Detection Functionality

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+import face_recognition
 def run_recognition():
     video_capture = cv2.VideoCapture(0)
 
@@ -8,8 +9,20 @@ def run_recognition():
 
     while True:
         ret, frame = video_capture.read()
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+        face_locations = []
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+
+        for (top, right, bottom, left) in face_locations:
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0))
 
         cv2.imshow('Face Recognition', frame)
 
